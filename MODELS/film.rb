19c9a743d009965c8cd9839e52543c11cd0f2faf
Derek Middlemiss <derekmiddlemiss@gmail.com>
2017-07-14 23:31:28 +0100

    Basic film class

--- MODELS/film.rb
+++ MODELS/film.rb
@@ -1,1 +1,13 @@
-require_relative( '' )
+require_relative( '../DB/sql_runner.rb' )
+
+def Film
+
+  attr_reader :id, :title, :price
+
+  def initialize( params )
+    @id = params[ 'id' ].to_i() if params[ 'id' ]
+    @title = params[ 'title' ]
+    @price = params[ 'price' ].to_f()
+  end
+
+end
